e7ae99d683cf5a7ee32b0983759e244338f17706
jesalmandalia <jesalmandalia1@gmail.com>
2023-10-17 17:13:07 +0100

    Creates a DAG that runs a Databricks notebook

---
+++ 0a48d8473ced_dag.py
@@ -0,0 +1,45 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+#Define params for Submit Run Operator
+notebook_task = {
+    'notebook_path': '/Users/jesalmandalia1@gmail.com/mount_s3_bucket',
+}
+
+
+#Define params for Run Now Operator
+notebook_params = {
+    "Variable":5
+}
+
+
+default_args = {
+    'owner': 'jesal',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+with DAG('databricks_dag',
+    # should be a datetime format
+    start_date=datetime.now(),
+    # check out possible intervals, should be a string
+    schedule_interval='@once',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        # the connection we set-up previously
+        databricks_conn_id='databricks_default',
+        existing_cluster_id='1108-162752-8okw8dgg',
+        notebook_task=notebook_task
+    )
+    opr_submit_run
